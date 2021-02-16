3e53b78011bfbbd42d4e07915514b19c9c0c0128
heet <54179610+heet05@users.noreply.github.com>
2021-02-16 17:44:47 +0530

    new add thig

--- app/src/main/java/com/example/dhobijunction/activity/PaymentActivity.java
+++ app/src/main/java/com/example/dhobijunction/activity/PaymentActivity.java
@@ -1,79 +1,88 @@
 package com.example.dhobijunction.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dhobijunction.R;
-import com.razorpay.Checkout;
-import com.razorpay.PaymentResultListener;
+import com.payumoney.core.PayUmoneySdkInitializer;
+import com.payumoney.sdkui.ui.utils.PayUmoneyFlowManager;
 
-import org.json.JSONObject;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-public class PaymentActivity extends AppCompatActivity implements PaymentResultListener {
+public class PaymentActivity extends AppCompatActivity {
+    String key = "ub4zyyCk";
+    String txnid = "1";
+    String amount = "10";
+    String productinfo = "Dhobi Junction";
+    String firstname = "Faizan";
+    String email = "faizanshaikh654933@gmail.com";
+    String salt = "lmBlYqpa5L";
+    String merchantId = "7380570";
+    String serverCalculatedHash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         getSupportActionBar().hide();
-        Checkout.preload(getApplicationContext());
-        startPayment();
+        String hashSequence = key + "|" + txnid + "|" + amount + "|" + productinfo + "|" + firstname + "|" + email + "|||||||||||" + salt;
+        serverCalculatedHash = hashCal("SHA-512", hashSequence);
 
+        PayUmoneySdkInitializer.PaymentParam.Builder builder = new
+                PayUmoneySdkInitializer.PaymentParam.Builder();
+        builder.setAmount(amount)                          // Payment amount
+                .setTxnId(txnid)                                             // Transaction ID
+                .setPhone("7405719399")                                           // User Phone number
+                .setProductName("Dhobi Junction")                   // Product Name or description
+                .setFirstName("Faizan")                              // User First name
+                .setEmail(email)                                            // User Email ID
+                .setsUrl("https://www.payumoney.com/mobileapp/payumoney/success.php")                    // Success URL (surl)
+                .setfUrl("https://www.payumoney.com/mobileapp/payumoney/failure.php")     //Failure URL (furl)
+                .setUdf1("")
+                .setUdf2("")
+                .setUdf3("")
+                .setUdf4("")
+                .setUdf5("")
+                .setUdf6("")
+                .setUdf7("")
+                .setUdf8("")
+                .setUdf9("")
+                .setUdf10("")
+                .setIsDebug(false)                              // Integration environment - true (Debug)/ false(Production)
+                .setKey(key)                        // Merchant key
+                .setMerchantId(merchantId);
 
-    }
-    public void startPayment() {
-
-        /**
-         * Instantiate Checkout
-         */
-        Checkout checkout = new Checkout();
-        checkout.setKeyID("rzp_test_VNnn5T54H6d66L");
-        /**
-         * Set your logo here
-         */
-        checkout.setImage(R.drawable.ccp_down_arrow);
-
-        /**
-         * Reference to current activity
-         */
-        final Activity activity = this;
-
-        /**
-         * Pass your payment options to the Razorpay Checkout as a JSONObject
-         */
+        PayUmoneySdkInitializer.PaymentParam paymentParam = null;
         try {
-            JSONObject options = new JSONObject();
-
-            options.put("name", "Merchant Name");
-            options.put("description", "Reference No. #123456");
-            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
-            options.put("order_id", "1");//from response of step 3.
-            options.put("theme.color", "#3399cc");
-            options.put("currency", "INR");
-            options.put("amount", "50000");//pass amount in currency subunits
-            options.put("prefill.email", "heetvadecha99@gmail.com");
-            options.put("prefill.contact","8780272970");
-            checkout.open(activity, options);
-
-        } catch(Exception e) {
-            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+            paymentParam = builder.build();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
+//set the hash
+        paymentParam.setMerchantHash(serverCalculatedHash);
+        // Invoke the following function to open the checkout page.
+        PayUmoneyFlowManager.startPayUMoneyFlow(
+                paymentParam,
+                this,
+                R.style.AppTheme_default,
+                true);
     }
 
-
-    @Override
-    public void onPaymentSuccess(String s) {
-        Toast.makeText(this, "payment Success", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onPaymentError(int i, String s) {
-        Log.d("TAG", "onPaymentError: "+s);
-        Toast.makeText(this, "error"+s, Toast.LENGTH_SHORT).show();
+    public static String hashCal(String type, String hashString) {
+        StringBuilder hash = new StringBuilder();
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance(type);
+            messageDigest.update(hashString.getBytes());
+            byte[] mdbytes = messageDigest.digest();
+            for (byte hashByte : mdbytes) {
+                hash.append(Integer.toString((hashByte & 0xff) + 0x100, 16).substring(1));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hash.toString();
     }
 }
