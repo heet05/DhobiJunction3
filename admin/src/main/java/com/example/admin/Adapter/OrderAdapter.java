8c7582cfd3913ea86f5c1197a78fa6657590db01
heetv <heetvadecha99@gmail.com>
2021-03-20 23:14:03 +0530

    add new

--- admin/src/main/java/com/example/admin/Adapter/OrderAdapter.java
+++ admin/src/main/java/com/example/admin/Adapter/OrderAdapter.java
@@ -25,6 +25,10 @@ import com.example.admin.activity.OrderdetailActivity;
 import com.example.admin.activity.Sub_CagtegoryActivity;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -38,7 +42,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class OrderAdapter extends FirestoreRecyclerAdapter<orderModel,OrderAdapter.ViewHolder>{
+public class OrderAdapter extends FirestoreRecyclerAdapter<orderModel, OrderAdapter.ViewHolder> {
     /**
      * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
      * FirestoreRecyclerOptions} for configuration options.
@@ -50,10 +54,11 @@ public class OrderAdapter extends FirestoreRecyclerAdapter<orderModel,OrderAdapt
     Context context;
     List<DeliveryModel> list;
     List<String> deliveryList = new ArrayList<>();
-    DeliveryModel deliveryModel1;
+DeliveryModel deliveryModel;
+    //DeliveryModel deliveryModel1;
     public OrderAdapter(OrderActivity orderActivity, @NonNull FirestoreRecyclerOptions<orderModel> options, OrderActivity activity) {
         super(options);
-        this.context=orderActivity;
+        this.context = orderActivity;
     }
 
     @Override
@@ -72,75 +77,103 @@ public class OrderAdapter extends FirestoreRecyclerAdapter<orderModel,OrderAdapt
         holder.linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(context,OrderdetailActivity.class);
-                intent.putExtra("Model",model);
+                Intent intent = new Intent(context, OrderdetailActivity.class);
+                intent.putExtra("Model", model);
                 context.startActivity(intent);
             }
         });
-      FirebaseFirestore.getInstance().collection("deliveryboy").addSnapshotListener(new EventListener<QuerySnapshot>() {
+
+
+        FirebaseFirestore.getInstance().collection("deliveryboy").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (value !=null && !value.isEmpty()) {
-                    list =value.toObjects(DeliveryModel.class);
-                    for (int i=0;i<list.size();i++){
 
+                if (value != null && !value.isEmpty()) {
+
+                    list = value.toObjects(DeliveryModel.class);
+
+                    for (int i = 0; i < list.size(); i++) {
+                        if (deliveryList.size()<list.size())
                         deliveryList.add(list.get(i).getName());
-
 
                     }
 
 
-                    ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(context,R.layout.support_simple_spinner_dropdown_item,deliveryList);
-                   holder.spinner.setAdapter(arrayAdapter);
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context, R.layout.support_simple_spinner_dropdown_item, deliveryList);
+
+                    holder.spinner.setAdapter(arrayAdapter);
+
 
                 }
+
+
+
             }
         });
-      holder.imageButton.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              FirebaseFirestore.getInstance().collection("deliveryboy" ).document().collection("Order").add(model).addOnSuccessListener(documentReference -> {
-                  String docId = documentReference.getId();
-                  Map<String, Object> map = new HashMap<>();
-                  map.put("oid", docId);
-                  documentReference.update(map).addOnSuccessListener(aVoid -> {
-                      Toast.makeText(context, "success", Toast.LENGTH_SHORT).show();
-                  }).addOnFailureListener(e -> {
-                      Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                  });
-              }).addOnFailureListener(e -> {
-                  Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        holder.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DeliveryModel deliveryModel = new DeliveryModel();
+                deliveryModel.getDid();
 
-              });
-          }
-      });
+               /* FirebaseFirestore.getInstance().collection("deliveryboy").whereEqualTo("did",holder.spinner.getSelectedItem()).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        FirebaseFirestore.getInstance().collectionGroup("Order").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                            }
+                        });
+                    }
+                });*/
+             FirebaseFirestore.getInstance().collection("deliveryboy").whereEqualTo("name",holder.spinner.getSelectedItem().toString()).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                 @Override
+                 public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                     value.getDocuments().get(0).getReference().collection("Order").add(model).addOnSuccessListener(documentReference -> {
+                         String docId = documentReference.getId();
+                         Map<String, Object> map = new HashMap<>();
+                         map.put("oid", docId);
+                         documentReference.update(map).addOnSuccessListener(aVoid -> {
+                             Toast.makeText(context, "success", Toast.LENGTH_SHORT).show();
+                         }).addOnFailureListener(e -> {
+                             Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                         });
+                     }).addOnFailureListener(e -> {
+                         Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                     });
+                 }
+             });
+            }
+        });
 
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.order_item,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.order_item, parent, false);
         return new ViewHolder(view);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textView, date,name,number,address,email,payment;
+        TextView textView, date, name, number, address, email, payment;
         LinearLayout linearLayout;
         Spinner spinner;
         ImageButton imageButton;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            name=itemView.findViewById(R.id.Order_name);
-            number=itemView.findViewById(R.id.Order_number);
-            payment=itemView.findViewById(R.id.Order_payment);
-            address=itemView.findViewById(R.id.Order_address);
-            email=itemView.findViewById(R.id.Order_email);
+            name = itemView.findViewById(R.id.Order_name);
+            number = itemView.findViewById(R.id.Order_number);
+            payment = itemView.findViewById(R.id.Order_payment);
+            address = itemView.findViewById(R.id.Order_address);
+            email = itemView.findViewById(R.id.Order_email);
             date = itemView.findViewById(R.id.Order_date);
             textView = itemView.findViewById(R.id.Order_total);
             linearLayout = itemView.findViewById(R.id.Order_click);
-            imageButton=itemView.findViewById(R.id.delvered);
-            spinner=itemView.findViewById(R.id.sp1);
+            imageButton = itemView.findViewById(R.id.delvered);
+            spinner = itemView.findViewById(R.id.sp1);
 
         }
     }
