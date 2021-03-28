0a4d3927507d3f924324e157555e8276f542d345
heetv <heetvadecha99@gmail.com>
2021-03-28 17:08:36 +0530

    add new

--- delivery_boy/src/main/java/com/example/delivery_boy/OrderDetailActivity.java
+++ delivery_boy/src/main/java/com/example/delivery_boy/OrderDetailActivity.java
@@ -1,13 +1,20 @@
 package com.example.delivery_boy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class OrderDetailActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -26,8 +33,26 @@ public class OrderDetailActivity extends AppCompatActivity {
         cancelorder = findViewById(R.id.order_detail_button1);
         textView = findViewById(R.id.Order_detail_total);
         Model = (OrderModel) getIntent().getSerializableExtra("Model");
-
-
+        pref=getSharedPreferences("deliveryBoy",MODE_PRIVATE);
+        cancelorder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseFirestore.getInstance().collection("deliveryboy").whereEqualTo("did",pref.getString("did","")).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                    if (value!=null && !value.isEmpty()){
+                        value.getDocuments().get(0).getReference().collection("Order").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                value.getDocuments().get(0).getReference().delete();
+                                finish();
+                            }
+                        });
+                    }
+                    }
+                });
+            }
+        });
         textView.setText(Model.getTotal());
         adapter = new OrderDetailAdapter(this, Model.getModelList());
         recyclerView.setAdapter(adapter);
